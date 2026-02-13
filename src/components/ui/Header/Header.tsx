40fb302d4e8e64a3e8ae13f44836ae286e4c7e06
Aquid0 <aquidmatthewla@gmail.com>
2026-02-13 13:35:47 +0000

    feat(modal): modal component

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState, useRef } from 'react';
-import { Popover } from '../Popover/Popover';
+import { Popover } from '../Popover';
+import { Modal } from '../Modal';
 
 export const Header = () => {
   const [isScrollingDown, setIsScrollingDown] = useState(false);
   const lastScrollTopRef = useRef(0);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [openLogin, setOpenLogin] = useState(false);
 
   const [isLoggedIn, setIsLoggedOn] = useState(false); // Placeholder for authentication state
 
@@ -53,37 +55,61 @@ export const Header = () => {
     </div>
   )
 
+  const loginContent = (
+    <div className="flex flex-col gap-4">
+      <input
+        className="bg-zinc-200 dark:bg-zinc-700 rounded px-2 py-1 text-sm text-zinc-900 dark:text-zinc-100"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="bg-zinc-200 dark:bg-zinc-700 rounded px-2 py-1 text-sm text-zinc-900 dark:text-zinc-100"
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+    </div>
+  )
+
   return (
-    <div
-      className={`px-17 min-w-fit dark:bg-zinc-900 bg-zinc-100 h-12 fixed w-full z-20 top-0 start-0 border-b border-zinc-300 dark:border-zinc-700 ${isScrollingDown ? '-translate-y-12' : 'translate-y-0'} transition-transform duration-175`}
-    >
-      <div 
-        className="mx-auto h-full w-full max-w-6xl flex items-center justify-between gap-5"
+    <>
+      <div
+        className={`px-17 min-w-fit dark:bg-zinc-900 bg-zinc-100 h-12 fixed w-full z-20 top-0 start-0 border-b border-zinc-300 dark:border-zinc-700 ${isScrollingDown ? '-translate-y-12' : 'translate-y-0'} transition-transform duration-175`}
       >
-        <div className="flex items-center text-xl font-semibold text-zinc-900 dark:text-zinc-100">
-          Library
-        </div>
-        <div className="flex items-center">{/* Center section */}</div>
-        <div className="flex items-center min-w-fit">{/* Right section */}
-          {
-            isLoggedIn ? (
-              <Popover content={profileContent}>
-                <div className="h-8 w-8 rounded-full bg-zinc-300 dark:bg-zinc-700 hover:bg-zinc-400 dark:hover:bg-zinc-600 flex items-center justify-center text-sm text-zinc-500 dark:text-zinc-400 transition-colors duration-200">
-                  U
+        <div 
+          className="mx-auto h-full w-full max-w-6xl flex items-center justify-between gap-5"
+        >
+          <div className="flex items-center text-xl font-semibold text-zinc-900 dark:text-zinc-100">
+            Library
+          </div>
+          <div className="flex items-center">{/* Center section */}</div>
+          <div className="flex items-center min-w-fit">{/* Right section */}
+            {
+              isLoggedIn ? (
+                <Popover content={profileContent}>
+                  <div className="h-8 w-8 rounded-full bg-zinc-300 dark:bg-zinc-700 hover:bg-zinc-400 dark:hover:bg-zinc-600 flex items-center justify-center text-sm text-zinc-500 dark:text-zinc-400 transition-colors duration-200">
+                    U
+                  </div>
+                </Popover> 
+                ) : 
+                <div className="flex gap-2 w-40 items-center">
+                  <button className="flex-1 bg-gray-300 text-black px-3 py-1 rounded text-sm text-nowrap font-bold">
+                    Sign Up
+                  </button>
+                  <button className="flex-1 bg-blue-600 text-white px-3 py-1 rounded text-sm text-nowrap font-bold" onClick={() => setOpenLogin(true)}>
+                    Login
+                  </button>
                 </div>
-              </Popover> 
-              ) : 
-              <div className="flex gap-2 w-40 items-center">
-                <button className="flex-1 bg-gray-300 text-black px-3 py-1 rounded text-sm text-nowrap font-bold">
-                  Sign Up
-                </button>
-                <button className="flex-1 bg-blue-600 text-white px-3 py-1 rounded text-sm text-nowrap font-bold">
-                  Login
-                </button>
-              </div>
-          }
+            }
+          </div>
         </div>
       </div>
-    </div>
+      { openLogin ? (
+        <Modal title={"Login"} onClose={() => setOpenLogin(false)} isOpen={openLogin} content={loginContent} />
+      ) : null
+      }
+    </>
   );
 };
---
+++ src/components/ui/Modal/Modal.tsx
@@ -0,0 +1,42 @@
+type ModalProps = {
+    title?: React.ReactNode;
+    content: React.ReactNode;
+    footerActions?: React.ReactNode[];
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export const Modal = ({ title, content, footerActions, isOpen, onClose }: ModalProps) => {
+    if (!isOpen) return null;
+
+    return (
+        <>
+            {/* Modal backdrop */}
+            <div className="w-full h-full fixed top-0 left-0 flex bg-black/75 items-center justify-center z-50" onClick={onClose}>
+                {/* Modal content */}
+                <div className="flex flex-col p-5 rounded-lg bg-white dark:bg-zinc-900" onClick={(e) => e.stopPropagation()}>
+                    {/* Close button */}
+                    <div className="flex justify-between items-center mb-4">
+                        {title}
+                        <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
+                            &times;
+                        </button>
+                    </div>
+
+                    {/* Main Content */}
+                    {content}
+
+                    {/* Footer Actions */}
+                    <div className="mt-4 flex justify-end"> 
+                        <button onClick={onClose} className="bg-blue-600 text-white px-3 py-1 rounded text-sm">
+                            Close
+                        </button>
+                        {footerActions && footerActions.map((action, index) => (
+                            <span key={index}>{action}</span>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
